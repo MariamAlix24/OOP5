8c49964b18ed8c72f9bb3aa401a3d9b2d6ac7582
MariamAlix24 <alimariem59@gmail.com>
2026-09-25 22:11:26 +0300

    Part 01 - Q1 Object Copying

--- OOP5/Program.cs
+++ OOP5/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-           
+            #region Part 01 — Theoretical Questions
+            #region Q1  Object Copying
+            //a)When you assign one object variable to another, you copy the reference (memory address) of the object, not the actual object itself.
+            //b)No, it does not create a new object. Both variables will point to the exact same object in memory, so modifying one affects the other.
+            //c)Copying a reference: Copies only the memory address. Both variables refer to the same object.
+            //-Copying an object: Creates a new, independent object in memory with the same values.
+            #endregion
+            #endregion
         }
     }
 }
